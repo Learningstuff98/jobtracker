92ea2baa3c868c734224b73b9c59fdc28a55d8bf
Andy Strube <andystrube64@gmail.com>
2021-10-28 16:37:50 -0700

    Applications can be edited

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -24,6 +24,20 @@ class ApplicationsController < ApplicationController
     redirect_to root_path
   end
 
+  def edit
+    @application = Application.find(params[:id])
+  end
+
+  def update
+    @application = Application.find(params[:id])
+    @application.update(application_params)
+    if @application.valid?
+      redirect_to application_path(@application)
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
   private
 
   def application_params
